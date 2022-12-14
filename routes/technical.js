e49128c9ecf96190e6c2b49f4f72416741dc2282
Simar Kaur <89771491+simarkaur28@users.noreply.github.com>
2022-12-15 00:25:41 +0530

    updated

--- routes/technical.js
+++ routes/technical.js
@@ -6,10 +6,10 @@ var mysql = require('mysql');
 // MySQL Connection
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-	password: "",
-    database: 'csiApp'
+    host:'3.110.128.94',
+	user: "csi",
+	password: "csi",
+    	database:'csiApp'
 });
 connection.connect(function(err) {
     if (!err) {
