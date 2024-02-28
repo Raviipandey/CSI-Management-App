d6a3f3bd2dcd586c54cc550643d813b4f41ff304
PurpleVen <vendra0408@gmail.com>
2024-02-29 00:39:05 +0530

    web ui alignment and dynamic dashboard cards added

--- routes/web-app/proposal.js
+++ routes/web-app/proposal.js
@@ -3,6 +3,8 @@ const session = require("express-session");
 const path = require("path");
 const { request } = require("http");
 const { response } = require("express");
+const cors = require('cors');
+
 
 module.exports = {
     get: (request, response) => {
@@ -77,6 +79,103 @@ module.exports = {
     
         })
     
+    },
+    countApprovedProposals: (request, response) => {
+        console.log("Received request for /countApprovedProposals");
+    
+        connection.query("SELECT COUNT(*) AS count FROM core_proposals_manager WHERE proposals_status = 3", function (error, results, fields) {
+            if (error) {
+                console.error("Error fetching count of approved proposals:", error);
+                response.status(500).json({message: "Error fetching count of approved proposals", error: error});
+                return;
+            }
+            console.log("Query executed successfully.");
+            
+            if (results.length > 0) {
+                console.log("Count of approved proposals:", results[0].count);
+                response.json({
+                    count: results[0].count
+                });
+            } else {
+                console.log("No results found. Returning count as 0.");
+                response.json({
+                    count: 0
+                });
+            }
+        });
+    },
+    countRejectedProposals: (request, response) => {
+        console.log("Received request for /countRejectedProposals");
+    
+        connection.query("SELECT COUNT(*) AS count FROM core_proposals_manager WHERE proposals_status = -3", function (error, results, fields) {
+            if (error) {
+                console.error("Error fetching count of approved proposals:", error);
+                response.status(500).json({message: "Error fetching count of rejected proposals", error: error});
+                return;
+            }
+            console.log("Query executed successfully.");
+            
+            if (results.length > 0) {
+                console.log("Count of rejected proposals:", results[0].count);
+                response.json({
+                    count: results[0].count
+                });
+            } else {
+                console.log("No results found. Returning count as 0.");
+                response.json({
+                    count: 0
+                });
+            }
+        });
+    },
+    countApprovedSBCProposals: (request, response) => {
+        console.log("Received request for /countApprovedSBCProposals");
+    
+        connection.query("SELECT COUNT(*) AS count FROM core_proposals_manager WHERE proposals_status = 2", function (error, results, fields) {
+            if (error) {
+                console.error("Error fetching count of approved SBC proposals:", error);
+                response.status(500).json({message: "Error fetching count of approved proposals", error: error});
+                return;
+            }
+            console.log("Query executed successfully.");
+            
+            if (results.length > 0) {
+                console.log("Count of approved SBC proposals:", results[0].count);
+                response.json({
+                    count: results[0].count
+                });
+            } else {
+                console.log("No results found. Returning count as 0.");
+                response.json({
+                    count: 0
+                });
+            }
+        });
+    },
+    countRejectedSBCProposals: (request, response) => {
+        console.log("Received request for /countRejectedSBCProposals");
+    
+        connection.query("SELECT COUNT(*) AS count FROM core_proposals_manager WHERE proposals_status = -2", function (error, results, fields) {
+            if (error) {
+                console.error("Error fetching count of rejected proposals:", error);
+                response.status(500).json({message: "Error fetching count of rejected proposals", error: error});
+                return;
+            }
+            console.log("Query executed successfully.");
+            
+            if (results.length > 0) {
+                console.log("Count of rejected proposals:", results[0].count);
+                response.json({
+                    count: results[0].count
+                });
+            } else {
+                console.log("No results found. Returning count as 0.");
+                response.json({
+                    count: 0
+                });
+            }
+        });
     }
+    
 
 };
--- servercopy.js
+++ servercopy.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var app=express();
+const createError = require('http-errors');
 var bodyParser=require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -8,7 +9,37 @@ app.use(bodyParser.urlencoded({
 
 var cors=require('cors');
 app.use(cors());
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const oneDay = 1000 * 60 * 5;
+const router = express.Router();
+var flash = require("connect-flash");
+app.use(flash());
 
+//web app session
+app.use(
+    session({
+        resave: true,
+        saveUnitialized: true,
+        secret: "secret",
+        cookie: { maxAge: oneDay }
+    })
+);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.json({}))
+
+
+//all static files are mentioned here
+app.use("/views",express.static("views"));
+app.use("/jquery",express.static("jquery"));
+
+app.use(express.static(__dirname + "/views"));
 
 
 app.use('/creative', express.static('./creative'));
@@ -36,9 +67,72 @@ app.use('/publicity',publicity);
 app.use('/technical',technical);
 app.use('/report',report);
 
+//web app view
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//web app routes
+
+var authUser = require('./routes/web-app/authUser');
+var dashboard = require('./routes/web-app/dashboard');
+var proposal = require('./routes/web-app/proposal');
+var feedback = require('./routes/web-app/feedback');
+var minute = require('./routes/web-app/minutes');
+var technical = require('./routes/web-app/technical');
+var publicity = require('./routes/web-app/publicity');
+var addmembers = require('./routes/web-app/addmembers')
+const { request } = require('http');
+
+app.use('/authUser', authUser.post);
+app.use('/dashboard', dashboard.get);
+app.use('/pydata', dashboard.pydata);
+app.use('/confirmedall', dashboard.confirmall);
+app.use('/proposalData', proposal.get);
+app.use('/fetchall', proposal.fetchall);
+app.use('/fetchsingle', proposal.fetchsingle);
+app.use('/hodstatusapprove', proposal.hodstatusconf);
+app.use('/hodstatusreject', proposal.hodstatusrej);
+app.use('/feedbackData', feedback.get);
+app.use('/feedbackall', feedback.feedbackall);
+app.use('/feedbacksingle', feedback.feedbacksingle);
+app.use('/feedbackupdate', feedback.feedbackupdate);
+app.use('/minuteData', minute.get);
+app.use('/minuteall', minute.minuteall);
+app.use('/minutesingle', minute.minutesingle);
+app.use('/techData', technical.get);
+app.use('/techall', technical.techall);
+app.use('/publicityData', publicity.get);
+app.use('/publicityall', publicity.publicityall);
+app.use('/addmemberspage', addmembers.get)
+app.use('/addmembers', addmembers.addmembers)
+app.use('/countApprovedProposals', proposal.countApprovedProposals)
+app.use('/countRejectedProposals', proposal.countRejectedProposals)
+app.use('/countApprovedSBCProposals', proposal.countApprovedSBCProposals)
+app.use('/countRejectedSBCProposals', proposal.countRejectedSBCProposals)
+
+
 app.get("/",(req,res)=>{
-return res.send("Welcome to CSI-DBIT");
+    let session = req.session;
+        if (session.userid) {
+        res.redirect("/dashboard");
+        } else {
+            res.render(__dirname + "/views/login.ejs", {
+                Emsg : req.flash("Emsg")
+            });
+        }
 });
+
+
+app.get("/logout", (req, res, next)=>{
+    req.session.destroy();
+    res.redirect("/");
+
+})
+
+// app.get("/",(req,res)=>{
+// return res.send("Welcome to CSI-DBIT");
+// });
 
 //Port Listening
 app.listen(9000,(req,res)=>{
