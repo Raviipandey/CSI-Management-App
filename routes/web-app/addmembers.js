3305f91dfd078591558a8ad5788d353bb52135fe
PurpleVen <vendra0408@gmail.com>
2024-03-04 22:31:15 +0530

    feature page image upload and validation done, add members csv upload and multitab role and name list

--- routes/web-app/addmembers.js
+++ routes/web-app/addmembers.js
@@ -3,6 +3,11 @@ const session = require("express-session");
 const path = require("path");
 const { request } = require("http");
 const { response } = require("express");
+const multer = require('multer');
+const csv = require('fast-csv');
+const fs = require('fs');
+
+const upload = multer({ dest: 'server_uploads/csv_upload' });
 module.exports = {
     get: (request, response) => {
         var session = request.session;
@@ -19,8 +24,8 @@ module.exports = {
     
         // Step 1: Insert data into core_details to get core_id
         connection.query(
-            'INSERT INTO csiApp2022.core_details (core_role_id, core_en_fname, core_en_lname, core_rollno, core_mobileno, core_email, core_pwd, core_branch, core_class, core_acad_year) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            [role, first_name, last_name, roll_no, mob_no, email, password, branch, class_name, acad],
+            'INSERT INTO csiApp2022.core_details (core_role_id, core_en_fname, core_en_lname, core_rollno, core_mobileno, core_email, core_pwd, core_branch, core_class, core_acad_year, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [role, first_name, last_name, roll_no, mob_no, email, password, branch, class_name, acad, request.body.gender],
             (error, result) => {
                 if (error) {
                     console.error('Error inserting data into the core_details table:', error.message);
@@ -69,5 +74,91 @@ module.exports = {
                 });
             }
         });
+    },
+    fetchCoreMembers: (request, response) => {
+        const year = request.params.year; // Assuming you have defined ":year" as a route parameter
+        // Validate the year format to prevent SQL injection and ensure it's a valid year format
+        if (!/^\d{2}-\d{2}$/.test(year)) {
+            console.error("Invalid year format received", year);
+            response.status(400).json({message: "Invalid year format"});
+            return;
+        }
+    
+        console.log(`Received request for /members/${year}`);
+    
+        connection.query("SELECT cd.core_en_fname, crm.role_name FROM core_details AS cd JOIN core_role_master AS crm ON cd.core_role_id = crm.role_id WHERE core_acad_year = ?", [year], function (error, results, fields) {
+            if (error) {
+                console.error(`Error fetching core members for ${year}`, error);
+                response.status(500).json({message: `Error fetching core members for ${year}`, error: error});
+                return;
+            }
+            console.log(`Query executed successfully for ${year}.`);
+    
+            if (results.length > 0) {
+                console.log(`Members fetched for ${year}:`, results.length);
+                response.json({
+                    year: year,
+                    members: results.map(row => row.core_en_fname),
+                    role : results.map(row=>row.role_name)
+                });
+            } else {
+                console.log(`No results found for ${year}. Returning empty list.`);
+                response.json({
+                    year: year,
+                    members: []
+                });
+            }
+        });
+    },
+    uploadCSV: (request, response) => {
+        upload.single('csvFile')(request, response, function (err) {
+            if (err instanceof multer.MulterError) {
+                return response.status(500).json(err);
+            } else if (err) {
+                return response.status(500).json(err);
+            }
+    
+            const fileRows = [];
+            fs.createReadStream(request.file.path)
+                .pipe(csv.parse({ headers: true }))
+                .on('error', error => console.error(error))
+                .on('data', row => fileRows.push(row))
+                .on('end', () => {
+                    // Process the fileRows array
+                    fileRows.forEach(row => {
+                        const { role, first_name, last_name, roll_no, mob_no, email, password, branch, class_name, mem_strt, mem_end, acad, gender } = row;
+                        
+                        // Step 1: Insert data into core_details to get core_id
+                        connection.query(
+                            'INSERT INTO csiApp2022.core_details (core_role_id, core_en_fname, core_en_lname, core_rollno, core_mobileno, core_email, core_pwd, core_branch, core_class, core_acad_year, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                            [role, first_name, last_name, roll_no, mob_no, email, password, branch, class_name, acad, gender],
+                            (error, result) => {
+                                if (error) {
+                                    console.error('Error inserting data into the core_details table:', error);
+                                } else {
+                                    // Step 2: Use the obtained core_id to insert data into core_membership
+                                    const core_id = result.insertId;
+                                    connection.query(
+                                        'INSERT INTO csiApp2022.core_membership (core_id, membership_start_date, membership_end_date) VALUES (?, ?, ?)',
+                                        [core_id, mem_strt, mem_end],
+                                        (membershipError) => {
+                                            if (membershipError) {
+                                                console.error('Error inserting data into the core_membership table:', membershipError);
+                                            }
+                                        }
+                                    );
+                                }
+                            }
+                        );
+                    });
+    
+                    fs.unlinkSync(request.file.path); // Optionally delete the file after processing
+                    response.status(200).json({ success: true, message: 'CSV file has been processed successfully' });
+                });
+        });
     }
+    
+    
+    
+    
 };
--- routes/web-app/featurepage.js
+++ routes/web-app/featurepage.js
@@ -7,28 +7,44 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 const router = express.Router();
+var express = require('express');
+
 
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+  destination: (req, file, cb) => {
       // Define the destination directory. Adjust the path as needed.
       const targetPath = path.join(__dirname, '..', '..', 'server_uploads', 'images_dynamic');
       // Ensure the directory exists, or create it
       fs.mkdirSync(targetPath, { recursive: true });
       cb(null, targetPath);
-    },
-    filename: (req, file, cb) => {
+  },
+  filename: (req, file, cb) => {
       // Generate a new filename based on event name, date, and the original file extension
       const { event_name, event_date } = req.body;
       const date = new Date(event_date);
       const formattedDate = `${date.getDate()}_${date.getMonth() + 1}_${date.getFullYear()}`;
       const newFilename = `${event_name}_${formattedDate}${path.extname(file.originalname)}`;
       cb(null, newFilename);
+  }
+});
+
+// Initialize Multer with storage and file restrictions
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024 // 10 MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|HEIC)$/)) {
+      req.fileValidationError = 'Only image files are allowed!';
+      return cb(new Error('Only image files are allowed!'), false);
     }
-  });
-  // Initialize Multer with the defined storage configuration
-const upload = multer({ storage: storage });
+    cb(null, true);
+  }
+});
+
 
 router.get('/', (req, res, next) => {
     var session = req.session;
@@ -44,15 +60,19 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', upload.single('event_image'), (req, res) => {
-    // Your POST handler code
-    try {
-        // Since the file is saved by Multer, just return a success message
-        res.send('Image uploaded successfully.');
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('An error occurred.');
-      }
+  if (req.fileValidationError) {
+      return res.status(400).send(req.fileValidationError);
+  }
+
+  try {
+      // Since the file is saved by Multer, just return a success message
+      res.send('Image uploaded successfully.');
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred.');
+  }
 });
+
 
 module.exports = router;
 
